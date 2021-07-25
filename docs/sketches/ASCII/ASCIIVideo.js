3e4592febc28bbcdbc85f37d270ffbd1d03edf7d
htovars <56688304+htovars@users.noreply.github.com>
2021-07-25 22:16:55 +0000

    Deploying to gh-pages from  @ ee23add85a0083ec6cf50ed7e2606b8b9d1059cd 🚀

--- docs/sketches/ASCII/ASCIIVideo.js
+++ docs/sketches/ASCII/ASCIIVideo.js
@@ -1,5 +1,5 @@
 let theShader;
-let img;
+let video;
 let planeSide = 1000;
 let button;
 let debug;
@@ -9,7 +9,7 @@ let slider;
 function preload(){
   // load the shader
   theShader = loadShader('/vc/docs/sketches/ASCII/texture.vert','/vc/docs/sketches/ASCII/texture.frag');
-  img = loadImage("/vc/docs/sketches/ASCII/dogos.jpg");
+  video = createVideo("/vc/docs/sketches/LumaShader/Luma_Video/SpaceJam.mp4");
   var str = "";
   for (var i=0 ; i<10 ; i++){
     str = "/vc/docs/sketches/ASCII/A"+String(i+1)+".png";
@@ -21,9 +21,12 @@ function setup() {
   // shaders require WEBGL mode to work
   createCanvas(1000, 1000, WEBGL);
   textureMode(NORMAL);
+  video.loop();
+  video.hide();
+  video.volume(0);
+  noStroke();
   shader(theShader);
   // here we're using setUniform() to send our uniform values to the shader
-  theShader.setUniform("image", img);
   for (var i=0 ; i<10 ; i++){
     str = "A"+String(i+1);
     theShader.setUniform(str, symbols[i]);
@@ -41,6 +44,7 @@ function setup() {
 }
 
 function draw() {
+  theShader.setUniform("image", video);
   theShader.setUniform("resolution", slider.value());
   showPixel.html('px '+slider.value());
   background(0);
@@ -58,8 +62,3 @@ function keyPressed(){
     theShader.setUniform("debug", debug);
   }
 }
-
-
-
-
-
