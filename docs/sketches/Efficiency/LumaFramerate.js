c1def67a95a1a98cbdb2d63d207cb86552ba21f9
Laura Natalia González Mora <lngonzalezm@unal.edu.co>
2021-07-25 19:44:58 -0500

    Update LumaFramerate.js

--- docs/sketches/Efficiency/LumaFramerate.js
+++ docs/sketches/Efficiency/LumaFramerate.js
@@ -1,40 +1,41 @@
-let theShader;
-let video;
-let planeSide = 400;
+let vid;
 
+ function setup() {
+   createCanvas(710, 400);
+   vid = createVideo(['/vc/docs/sketches/LumaShader/Luma_Video/SpaceJam.mp4'], onVideoLoad);
+}
 
-function preload(){
-  // load the shader
-  theShader = loadShader('/vc/docs/sketches/LumaShader/Luma_Ponderado/texture.vert','/vc/docs/sketches/LumaShader/Luma_Ponderado/texture.frag');
-  video = createVideo("/vc/docs/sketches/LumaShader/Luma_Video/SpaceJam.mp4");
-}
-function setup() {
-  // shaders require WEBGL mode to work
-  
-  createCanvas(400, 400, WEBGL);
-  video.hide();
-  video.volume(0);
-  video.autoplay(true);
-  video.size(710, 400);
-  video.loop();
-  shader(theShader)
-  textureMode(NORMAL);
-  shader(theShader);
-  // here we're using setUniform() to send our uniform values to the shader
-}
-function draw() {
-  background(255);
+ function draw() {
+
+  background(250, 250, 250);
+
   textSize(20);
-  text("Frame con frameRate() = ");
+  
 
-  beginShape();
-  vertex(-planeSide/2, -planeSide/2, 0, 0); // esquina inferior izquierda
-  vertex(planeSide/2, -planeSide/2, 1, 0); // esquina inferior derecha
-  vertex(planeSide/2, planeSide/2, 1, 1); // esquina superior derecha
-  vertex(-planeSide/2, planeSide/2, 0, 1); // esquina superior izquierda
-  endShape(CLOSE);
-
-  theShader.setUniform("texture", video);
-  theShader.setUniform("textureWidth", 400.0);
-  theShader.setUniform("textureHeight", 400.0)
+  image(vid, 0, 50, 710, 400);
+  loadPixels();
+  const step = 1;
+  for (let y = 0; y < height; y+= step) {
+    for (let x = 0; x < width; x+= step) {
+       let index = 4 * (x + width * y);
+       let r = pixels[index];
+       let g = pixels[index + 1];
+       let b = pixels[index + 2];
+       let gamma = r * 0.299 + g * 0.587 + b * 0.0114;
+       let grayColor = color(gamma, gamma, gamma);
+       pixels[index] = gamma;
+       pixels[index + 1] = gamma;
+       pixels[index + 2] = gamma;
+      }
+  }
+  updatePixels();
+  text("Frame con frameRate() = " + frameRate().toFixed(3), 100, 30);
+}
+function onVideoLoad() {
+  vid.hide();
+  vid.play();
+  vid.volume(0);
+  vid.autoplay(true);
+  vid.size(710, 400);
+  vid.loop();
 }
